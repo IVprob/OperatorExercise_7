1f5723e29d8e40f872b9d2c821057da487d3d768
IVProb <jnoojinjr@gmail.com>
2021-03-26 09:45:55 -0500

    finished exercise 1 & 2 from lecture

--- OperatorExercise_7/Program.cs
+++ OperatorExercise_7/Program.cs
@@ -6,7 +6,26 @@ namespace OperatorExercise_7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Choose any two numbers.");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            DivisionRemainder(a, b);
+            AreaOfCircle(a);
+        }
+
+        static void DivisionRemainder(int a, int b)
+        {
+            int quotient = a / b;
+            int remainder = a % b;
+            Console.WriteLine($"{a}/{b} is equal to {quotient} and has a remainder of {remainder}");
+        }
+
+        static double AreaOfCircle(double r)
+        {
+            double area = Math.PI * Math.Pow(r, 2);
+            return area;
+            Console.WriteLine($"The Area of your circle is {area}.");
         }
     }
 }
